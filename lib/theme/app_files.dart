ec4f90580e82672843c37a7feeb8fa45b59b6eae
“Tatyana <korotyshtatyana@gmail.com>
2026-07-13 11:39:03 +0300

    add module_6 view

--- lib/theme/app_files.dart
+++ lib/theme/app_files.dart
@@ -8,6 +8,6 @@ class AppFiles {
   static const String module_3 = '$_module3Path/Профессия_инженера_конструктора_vs_инженера_технолога.pdf';
   static const String module_4 = '$_module4Path/Роль_инженера_энергетика_в_современной_энергетике.pdf';
   static const String module_5 = 'https://github.com/korotyshtatyana-cpu/portfolio_landing/raw/refs/heads/main/assets/module_5/MagLog%20City.docx';
-  static const String module_6 = '$_module6Path/Роль_инженера_энергетика_в_современной_энергетике.pdf';
+  static const String module_6 = 'https://github.com/korotyshtatyana-cpu/portfolio_landing/raw/refs/heads/main/assets/module_6/%D0%A1%D1%85%D0%B5%D0%BC%D0%B0_%D1%81%D1%82%D1%80%D0%BE%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D1%81%D1%82%D0%B2%D0%B0_%D0%BE%D0%B4%D0%BD%D0%BE%D1%8D%D1%82%D0%B0%D0%B6%D0%BD%D0%BE%D0%B3%D0%BE_%D0%B4%D0%BE%D0%BC%D0%B0.docx';
   static const String module_7 = 'https://github.com/korotyshtatyana-cpu/portfolio_landing/raw/refs/heads/main/assets/module_7/%D0%A1%D1%82%D0%B0%D1%80%D1%82%D0%B0%D0%BF%20%E2%80%9C%D0%94%D0%97-%D0%91%D0%BE%D1%82%E2%80%9D.docx';
 }
